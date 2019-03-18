f7d97f88bb236fa1192f0b2cf9d2b1dbd0812928
Jensen Santos <jensen.santos20@gmail.com>
2019-03-18 12:22:15 -0600

    Updates

---
+++ precache-manifest.d97aef222befefa4307d5652021064ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24245727bcb8e5175aef",
+    "url": "/disney/static/js/runtime~main.f1d1ea58.js"
+  },
+  {
+    "revision": "e666b032c00648a4bf9c",
+    "url": "/disney/static/js/main.28bfe86e.chunk.js"
+  },
+  {
+    "revision": "17d6eb59b6c003d2b289",
+    "url": "/disney/static/js/2.7baffc28.chunk.js"
+  },
+  {
+    "revision": "e666b032c00648a4bf9c",
+    "url": "/disney/static/css/main.35963f15.chunk.css"
+  },
+  {
+    "revision": "c98ac45b24b79d2e4e874514c703c05e",
+    "url": "/disney/index.html"
+  }
+];
