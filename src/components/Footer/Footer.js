2cc46511cda3eda8abe4a18db340c68e41017ff6
Jensen Santos <jensen.santos20@gmail.com>
2019-03-18 04:27:56 -0600

    configuring to github

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+// set the footer to a copyright string
+const Footer = props => (
+  <div className="footer">
+    <p class="text-muted text-center">Disney Memory Card Game</p>
+  </div>
+);
+
+export default Footer;
